24eeb1c174fa6bb0cfc8854b429bc438275100a9
BrettMayson <brett@mayson.io>
2025-02-08 08:45:26 -0600

    join servers

--- synixe.sh
+++ synixe.sh
@@ -3,6 +3,7 @@
 NAME=""
 MODS_LOCATION=""
 
+
 update() {
     rsync -ur -av --delete-after --progress -e "ssh -p 62" download@arma.synixe.contractors:/ $MODS_LOCATION
     echo "Mods Updated"
@@ -23,17 +24,27 @@ launch() {
         exit 1
     fi
 
-    # Construct the mod arguments for Steam
+    # Construct the mod arguments for Steam, relative to ARMA_LOCATION
     mod_args=""
     for folder in $mod_folders; do
         mod_args+="-mod=\"Z:$(realpath "$folder")\" "
     done
 
+    join_args=""
+    if [ -n "$SERVER" ]; then
+        join_args+="-connect=\"$SERVER\" "
+    fi
+    if [ -n "$PASSWORD" ]; then
+        join_args+="-password=\"$PASSWORD\" "
+    fi
+    if [ -n "$PORT" ]; then
+        join_args+="-port=\"$PORT\" "
+    fi
+
     # Launch Arma 3 via Steam with the mod arguments
     echo "Launching Arma 3"
-    steam -applaunch 107410 -nolauncher -name="$NAME" -skipIntro -noSplash -noPause $mod_args
+    steam -applaunch 107410 -nolauncher -name="$NAME" -skipIntro -noSplash -noPause $mod_args $join_args
 }
-
 if [ -z "$MODS_LOCATION" ]; then 
     echo "MODS_LOCATION is not set"
     exit 1
@@ -42,13 +53,23 @@ fi
 case $1 in
     update)
         update
+        exit 0
         ;;
-    launch)
-        update
-        launch
+    contracts)
+        SERVER="arma.synixe.contractors"
+        PORT="2302"
+        PASSWORD="literallyjusthitspacebar" 
+        ;;
+    training)
+        SERVER="arma.synixe.contractors"
+        PORT="2602"
+        PASSWORD="equals"
         ;;
     *)
-        echo "Usage: $0 {update|launch}"
+        echo "Usage: $0 {update|launch|training|contracts}"
         exit 1
         ;;
 esac
+
+update
+launch
